3c6895e6a54407bf195ef83a3fe8f20d8ccccd05
David Garza <dgarza0413@gmail.com>
2020-11-13 14:07:32 -0600

    vue init with component init

---
+++ src/components/CanvasBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test-class">
+    this is a div
+    <canvas></canvas>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "CanvasBox",
+};
+</script>
+
+<style scoped>
+.test-class {
+  height: 100%;
+  width: 100%;
+  background-color: aqua;
+}
+</style>
